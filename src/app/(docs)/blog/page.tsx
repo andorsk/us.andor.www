032ba9658e064a61f88c7cc95285b41e1f6fc6e5
Andor Kesselman <andor@magic-pro-3.local>
2024-03-06 22:32:37 -0800

    removed blog's page

--- src/app/(docs)/blog/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Blog() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex w-full flex-col items-center justify-between">
-        Stay tuned! Blog coming soon.
-      </div>
-    </main>
-  );
-}
