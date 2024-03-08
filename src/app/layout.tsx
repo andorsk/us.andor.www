34b849d6b6f61756925c7d7fb3438c2fcb7c3949
Andor Kesselman <andor@magic-pro-3.local>
2024-03-07 23:07:57 -0800

    added pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,49 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const APP_NAME = "Andor's Lab";
+const APP_DEFAULT_TITLE = "Andor's Lab";
+const APP_TITLE_TEMPLATE = "%s - PWA App";
+const APP_DESCRIPTION = "web5, ai, and random";
+
 export const metadata: Metadata = {
-  title: "andor.us",
-  description: "web5, ai, and random",
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
 };
 
 export default function RootLayout({
@@ -20,3 +57,7 @@ export default function RootLayout({
     </html>
   );
 }
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
+};
