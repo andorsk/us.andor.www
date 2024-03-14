0467c6071a213d28e31b37cee69315b0204c62fe
Andor Kesselman <andor@andor.us>
2024-03-13 22:57:10 -0700

    fixed header grid box layout

--- src/layouts/GridBoxLayout.tsx
+++ src/layouts/GridBoxLayout.tsx
@@ -17,12 +17,13 @@ export const GridBoxLayout = (props: GridBoxLayoutProps) => {
   return (
     <DocumentLayout>
       <div className="flex w-full flex-col items-center py-10 text-center">
+        <div className="opacity-50 text-sm">{props.header}</div>
         <div className="grid w-full grid-cols-1 md:grid-cols-3 text-center py-5">
           {props.links.map((item, index) => (
             <a
               key={index}
               href={item.url}
-              className="group  rounded-lg border border-transparent p-5 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+              className="group rounded-lg border border-transparent p-5 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
               target="_blank"
               rel="noopener noreferrer"
             >
