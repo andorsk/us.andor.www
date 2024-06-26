f37d30c6ee8fe62b40dbf35d920f51bba31ccdc7
Andor Kesselman <andor@andor.us>
2024-06-26 08:26:42 -0700

    working on services

---
+++ src/pages/services/data.ts
@@ -0,0 +1,26 @@
+type ServiceDescriptionType = {
+  name: string;
+  id: string;
+  description: string;
+};
+
+export const serviceDescriptions = [
+  {
+    name: "Engineering/Product Stabilization For Early Stage Startups",
+    id: "engineering-product-stabilization-for-early-stage-startups",
+    description:
+      "Are critical deadlines looming while your engineering team struggles with chaos? Trying to raise capital and keep the product afloat? I specialize in stepping into challenging environments—whether they lack structure or face significant disarray—and restoring order and efficiency. I have worked with multiple companies in the past where engineering teams have completely vanished on my clients, and I have come in fresh, stabilized the product and turned things around to operational. My background encompasses working with diverse teams and stacks across various organizations, enabling me to swiftly navigate and rectify complex situations.",
+  },
+  {
+    name: "Technical Evaluation for M&A Targets",
+    id: "technical-evaluation-for-ma-targets",
+    description:
+      "Considering an acquisition and need to evaluate the technical assets? I offer in-depth technical assessments for mergers and acquisitions. Having been on both sides of the equation—as an acquisition target and an acquirer—I bring a balanced perspective to each evaluation. My expertise spans AI, machine learning, scalable backend systems, front-end development, and more, equipping me to thoroughly analyze and advise on potential technical investments",
+  },
+  {
+    name: "Pure Engineering/Engineering Management Temporary Support",
+    id: "pure-engineering-engineering-management-temporary-support",
+    description:
+      "Need an IC for a specific project? Need an engineering manager? I offer project based services. I can code in Go, Typescript, Python, and have a little bit of experience with Rust. I have experience with Kubernetes (cloud and edge), ML pipelines, and can support the full stack. I have worked on multiple companies in the Web3 space, Retail AI space, and Decentralized Identity Space. I have a passion particularly for decentralization, but I can spread across other focuses when required.",
+  },
+] as ServiceDescriptionType[];
